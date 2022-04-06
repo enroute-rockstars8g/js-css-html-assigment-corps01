f25c9d2c51f7c9d754b2f27c53c3328adc38e417
Omar Corpus <omar.corpus@fulfillrayo.com>
2022-04-06 06:02:56 -0500

    working first version

---
+++ index.js
@@ -0,0 +1,39 @@
+function calc() {
+  const input_value = parseInt(document.getElementById("inputTaco").value);
+  if (document.getElementById("Radfib").checked) {
+    fibo(input_value);
+  }
+
+  if (document.getElementById("Radfac").checked) {
+    fac(input_value);
+  }
+
+  function printResult(result) {
+    praragraph = document.getElementById("resultContainer");
+    praragraph.innerText = result.join(",");
+  }
+
+  function fibo(input_value) {
+    const numberToShow = [];
+    let n1 = 0,
+      n2 = 1,
+      nextTerm;
+    for (let i = 1; i <= input_value; i++) {
+      numberToShow.push(n1);
+      nextTerm = n1 + n2;
+      n1 = n2;
+      n2 = nextTerm;
+    }
+    printResult(numberToShow);
+  }
+
+  function fac(input_value) {
+    const numberToShow = [];
+    let total = 1;
+    for (i = 1; i <= input_value; i++) {
+      numberToShow.push(total); 
+      total = total * i;
+    }
+    printResult(numberToShow);
+  }
+}
